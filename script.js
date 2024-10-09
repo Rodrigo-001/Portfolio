1e93f16511b7e2ea4b7f7f632d972c018366834c
rodrigoc01 <rodrigocamargo1234santos@gmail.com>
2024-10-09 19:30:35 -0300

    Adicionado efeito hover para link de curriculo e acompanhe projetos.

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let estilosOriginais = {}
+
 function aocarregar() {
     // Selecionando elementos para adicionar eventlistener
     const linksTopo = [
@@ -12,19 +14,49 @@ function aocarregar() {
         link.addEventListener("mouseenter", () => entrou(link));
         link.addEventListener("mouseleave", () => saiu(link));
     });   
-}
 
+    // Selecionando elementos para adicionar eventlistener
+    const linksComEfeitoGrow = [
+        document.getElementsByClassName("link-curriculo")[0],
+        document.getElementsByClassName("link-acompanhar")[0]
+    ];
+ 
+    linksComEfeitoGrow.forEach(link => {        
+        link.addEventListener("mouseenter", () => entrou(link));
+        link.addEventListener("mouseleave", () => saiu(link));
+    });
+}
 // Função para quando o mouse entrar no elemento
 function entrou(elemento) {    
-    elemento.style.backgroundColor = '#F2A900';
-    elemento.style.borderRadius = '18px';
-    elemento.style.padding = '6px';
-    elemento.style.color = '#FFF';
+    estilosOriginais = {
+        backgroundColor: elemento.style.backgroundColor,
+        borderRadius: elemento.style.borderRadius,
+        padding: elemento.style.padding,
+        color: elemento.style.color,
+        transform: elemento.style.transform
+    };
+    let idElemento = (elemento.id);
+    let classeDoElemento = elemento.className;   
+
+    //EFEITO PARA LINKS DE NAVEGAÇÃO DO TOPO
+    if (idElemento == "linkTopo" || idElemento == "linkSobre" || idElemento == "linkProjetos" || idElemento == "linkContatos") {        
+        elemento.style.backgroundColor = '#F2A900';
+        elemento.style.borderRadius = '18px';
+        elemento.style.padding = '6px';
+        elemento.style.color = '#FFF';
+    } else if(classeDoElemento == "link-curriculo" || classeDoElemento == "link-acompanhar") {     
+        elemento.style.transform = 'scale(1.05)';
+    }
+    
 }
 
 // Função para quando o mouse sair do elemento
-function saiu(elemento) {
-    elemento.style.backgroundColor = '#FFF';
-    elemento.style.borderRadius = '0px';  
-    elemento.style.color = '#000';
+function saiu(elemento) {    
+    elemento.style.backgroundColor = estilosOriginais.backgroundColor;
+    elemento.style.borderRadius = estilosOriginais.borderRadius;
+    elemento.style.padding = estilosOriginais.padding;
+    elemento.style.color = estilosOriginais.color;
+    elemento.style.transform = estilosOriginais.transform;
+    
+    
 }
