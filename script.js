638a4909bc98424c3d4e404fb3d8bbcdf5f3cf02
rodrigoc01 <rodrigocamargo1234santos@gmail.com>
2024-10-09 17:54:37 -0300

    Adicionado efeito hover no links do topo

--- script.js
+++ script.js
@@ -1,29 +1,30 @@
 function aocarregar() {
     // Selecionando elementos para adicionar eventlistener
-    const links = [
+    const linksTopo = [
         document.getElementById("linkTopo"),
         document.getElementById("linkSobre"),
         document.getElementById("linkProjetos"),
         document.getElementById("linkContatos")
     ];
 
-    // Adicionando eventlistener nos elementos
-    links.forEach(link => {
+    // Adicionando eventListeners nos links de navegação do cabeçalho
+    linksTopo.forEach(link => {
         link.addEventListener("mouseenter", () => entrou(link));
         link.addEventListener("mouseleave", () => saiu(link));
     });   
 }
 
 // Função para quando o mouse entrar no elemento
-function entrou(elemento) {
+function entrou(elemento) {    
     elemento.style.backgroundColor = '#F2A900';
     elemento.style.borderRadius = '18px';
     elemento.style.padding = '6px';
+    elemento.style.color = '#FFF';
 }
 
 // Função para quando o mouse sair do elemento
 function saiu(elemento) {
     elemento.style.backgroundColor = '#FFF';
-    elemento.style.borderRadius = '0px';
-
+    elemento.style.borderRadius = '0px';  
+    elemento.style.color = '#000';
 }
